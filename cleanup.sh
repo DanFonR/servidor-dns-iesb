6d7fe9fb4135bedfdd31e394c0656518435fe115
potatoo14 <92479313+potatoo14@users.noreply.github.com>
2025-11-24 01:46:46 -0300

    tmp2

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+docker rm -f "$(docker ps -aq)"
 docker compose -f docker/docker-compose.yml --env-file template.env down --volumes --remove-orphans
 docker image prune -a -f
 docker volume prune -f
